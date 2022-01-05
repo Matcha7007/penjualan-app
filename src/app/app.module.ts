f96ce9ce58195eae9e5b2883506f5ed9242341c6
Matcha7007 <matchaaddict007@gmail.com>
2022-01-05 10:14:42 +0700

    ada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserSigninComponent } from './templates/users/user-signin/user-signin.component';
+import { UserSignupComponent } from './templates/users/user-signup/user-signup.component';
+import { NavBarComponent } from './templates/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSigninComponent,
+    UserSignupComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
