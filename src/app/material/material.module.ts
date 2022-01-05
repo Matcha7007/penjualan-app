666c963c9301ced4d5666d1ad83d373698dfb716
Matcha7007 <matchaaddict007@gmail.com>
2022-01-05 10:23:03 +0700

    edit material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -19,23 +18,14 @@ const MaterialComponent = [
   MatIconModule,
   MatGridListModule
 ]
-=======
-import { CommonModule } from '@angular/common';
-
-
->>>>>>> f96ce9ce58195eae9e5b2883506f5ed9242341c6
 
 @NgModule({
   declarations: [],
   imports: [
-<<<<<<< HEAD
     MaterialComponent
   ],
   exports: [
     MaterialComponent
-=======
-    CommonModule
->>>>>>> f96ce9ce58195eae9e5b2883506f5ed9242341c6
   ]
 })
 export class MaterialModule { }
